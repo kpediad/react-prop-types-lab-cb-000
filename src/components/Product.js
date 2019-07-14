8fb8f170dc66a6fad9dbad53224dc16016199509
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-14 14:20:46 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,22 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+ 
+  render() {
+    return (
+      <div className="product">
+
+      </div>
+    );
+  }
+};
+
+Product.defaultProps = {
+  
+};
+ 
+Product.propTypes = {};
+
+
