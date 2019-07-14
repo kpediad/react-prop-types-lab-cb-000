33d5170f1ac0edb08a52caeac9d68c86738ed93e
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-14 14:33:16 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,14 +7,25 @@ class Product extends React.Component {
   render() {
     return (
       <div className="product">
-
+        <h2>Name: {this.prop.name}</h2>
+        <h3>Producer: {this.prop.producer}</h3>
+        <h3>Watermark: {this.prop.hasWatermark ? "Yes" : "No"}</h3>
+        <h3>Color: {this.prop.color}</h3>
+        <h3>Weight: {this.prop.weight}</h3>
       </div>
     );
   }
 };
 
 Product.defaultProps = {
-
+  hasWatermark: false
 };
 
-Product.propTypes = {};
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.string.isRequired,
+  weight: PropTypes.
+  
+};
